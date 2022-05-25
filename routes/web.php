b80a3173465bef18de5bf74323977f9f41d583f6
Nguyễn Thành Đức <ducthanh.developer@gmail.com>
2022-05-25 17:29:36 +0700

    admin template

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth
 Route::get('/', function () {
     return view('Auth.account.profile');
 });
-Route::get('/productdetails', function(){
+Route::get('/productdetails', function () {
     return view('Auth.product_details.productdetails');
 });
-Route::get('/login', function(){
+Route::get('/login', function () {
     return view('Auth.account.login');
 });
-Route::get('/register', function(){
+Route::get('/register', function () {
     return view('Auth.account.register');
 });
-Route::get('/profile', function(){
+Route::get('/profile', function () {
     return view('Auth.account.profile');
 });
+
+// Admin
+
+Route::get('/admintrator', function () {
+    return view('Admin.dashboard.dashboard');
+})->name('dashboard');
+Route::get('/admintrator/user', function () {
+    return view('Admin.user.user');
+})->name('user');
