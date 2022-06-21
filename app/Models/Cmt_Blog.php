6013ccad0c0cf197dbb4b03f465df7791820ba71
NguyenLamTruong-DEVWEB <nguyenlamtruong.dev@gmail.com>
2022-06-21 18:39:42 +0700

    edit database and manager comment blog , product

---
+++ app/Models/Cmt_Blog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cmt_Blog extends Model
+{
+    protected $table = "t_commentpost";
+    protected $primaryKey = "id";
+}
---
+++ app/Models/Cmt_Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cmt_Product extends Model
+{
+   protected $table = "t_commentproduct";
+   protected $primakey = "id";
+}
