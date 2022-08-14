e950f88adc0d77ef46919a93b35695e630f5f6b1
wedontalk <duy09092001@gmail.com>
2022-08-14 11:54:40 +0700

    update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,8 @@ use App\Models\AccountModel;
 use App\Models\OrderDeTailModel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class ProfileController extends Controller
 {
@@ -27,21 +29,43 @@ class ProfileController extends Controller
         $order = OrderModel::find($id);
         $order->m_status = "3";
         $order->update();
-        return redirect()->back()->with('alert_success', 'Hủy đơn hàng thành công thành công.');
+        return redirect()->route('updateProfile')->with('alert_success', 'Hủy đơn hàng thành công thành công.');
     }
     // Cập nhật thông tin profile
-    public function update(Request $request, $id)
+    public function updateProfile(Request $request)
     {
-        $updated = accountModel::find($id);
+        $updated = accountModel::find(\Auth::user()->id);
         $updated->email = $request->email;
         $updated->phone = $request->phone;
         $updated->m_address = $request->m_address;
         if($updated->save()){
-            return redirect()->back()->with('alert_success', 'Cập nhật thông tin thành công.');}
+            return redirect()->route('updateProfile')->with('alert_success', 'Cập nhật thông tin thành công.');}
     }
-    public function updateMK(Request $request, $id)
-    {
-
+    
+    public function doithongtinuser(Request $request){
+        $updated = accountModel::find(\Auth::user()->id);
+        $updated->email = $request->email;
+        $updated->phone = $request->phone;
+        $updated->m_address = $request->m_address;
+        if($updated->save()){
+            return redirect()->back()->with('alert_success', 'Cập nhật thông tin thành công.');
+        }
+    }
+    public function doimatkhauuser(Request $request){
+        $id = $request->id;
+        $data = $request->all();
+        $passold = $data['matkhaucu'];
+        // dd($data['matkhaucu']);
+        // dd($data['xacnhanmatkhau']);
+        if(Hash::check($passold, \Auth::user()->password)){
+            if($data['matkhaumoi'] == $data['xacnhanmatkhau']){
+                $updated = accountModel::find(\Auth::user()->id);
+                $updated->password = Hash::make($data['matkhaumoi']);
+                if($updated->save()){
+                    echo 'Đổi mật khẩu thành công!';
+                }       
+            }
+        }
     }
 
 }
